414a20fd2de5ee7b4bf0e26a23b818f597e1c8b6
Aleksandr Bogoslovskiy <balics@mail.ru>
2024-04-09 19:41:19 +0300

    delete redundant code

--- algorithms/algorithm01/src/algorithm01/Main.java
+++ algorithms/algorithm01/src/algorithm01/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	static String toCamelCase(String s){
 		String[] strs = s.split("[-_]");
 		return Arrays.stream(strs)
-				.reduce((s1,s2) -> s1 + s2.substring(0, 1).toUpperCase() + s2.substring(1, s2.length())).get();
+				.reduce((s1,s2) -> s1 + s2.substring(0, 1).toUpperCase() + s2.substring(1)).get();
 	}
 	
 }
