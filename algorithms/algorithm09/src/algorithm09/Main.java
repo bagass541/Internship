67df3207977fbd1e635bd4e8dd4dd128b8b3ec17
Aleksandr Bogoslovskiy <balics@mail.ru>
2024-04-12 14:55:07 +0300

    make the code shorter

--- algorithms/algorithm09/src/algorithm09/Main.java
+++ algorithms/algorithm09/src/algorithm09/Main.java
@@ -1,7 +1,5 @@
 package algorithm09;
 
-import java.util.regex.Pattern;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -12,10 +10,7 @@ public class Main {
 	}
 	
 	public static boolean alphanumeric(String s){
-		if(s.isEmpty()) return false;
-		
-		Pattern pattern = Pattern.compile("[a-zA-Z0-9]+");
-		return pattern.matcher(s).matches();
+		return s.matches("[A-Za-z0-9]+");
 	}
 
 }
