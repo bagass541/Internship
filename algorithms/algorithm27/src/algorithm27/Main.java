2af3154dd8a6ef9e7d27e1e9303d8080a8129647
Alexandr Bogoslovskiy <balics@mail.ru>
2024-04-26 00:11:30 +0300

    delete redundant code

--- algorithms/algorithm27/src/algorithm27/Main.java
+++ algorithms/algorithm27/src/algorithm27/Main.java
@@ -1,13 +1,9 @@
 package algorithm27;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Stack;
-
-
 
 public class Main {
 
@@ -19,24 +15,6 @@ public class Main {
 		Arrays.stream(dirReduc(new String[] {"\"NORTH\"", "\"EAST\"", "\"WEST\"", "\"SOUTH\"", "\"WEST\"", "\"NORTH\""})).forEach(s -> System.out.println(s));
 	}
 
-//    public static String[] dirReduc(String[] arr) {
-//    	List<String> res = new ArrayList<>();
-//		for (int i = 0; i < arr.length - 1; i++) {
-//			System.out.println("arr " + i + " " + arr[i]);
-//			System.out.println("arr " + i + 1 + " " + arr[i + 1]);
-//			if ((arr[i].equals("\"NORTH\"") && arr[i + 1].equals("\"SOUTH\""))
-//					|| (arr[i].equals("\"SOUTH\"") && arr[i + 1].equals("\"NORTH\""))
-//					|| (arr[i].equals("\"WEST\"") && arr[i + 1].equals("\"EAST\""))
-//					|| (arr[i].equals("\"EAST\"") && arr[i + 1].equals("\"WEST\""))) {
-//				i += 1;
-//			} else {
-//				res.add(arr[i]);
-//			}
-//		}
-//		
-//		return res.toArray(new String[res.size()]);
-//    }
-	
 	 public static String[] dirReduc(String[] arr) {
 		 Stack<String> stack = new Stack<>();
 		 Map<String, String> map = new HashMap<>();
