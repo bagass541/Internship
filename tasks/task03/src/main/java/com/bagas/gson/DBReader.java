a88ae130ecffddbbdd4cbaadf910528723e3b369
Aleksandr Bogoslovskiy <balics@mail.ru>
2024-05-02 20:58:56 +0300

    realize writing to json files

--- tasks/task03/src/main/java/com/bagas/gson/DBReader.java
+++ tasks/task03/src/main/java/com/bagas/gson/DBReader.java
@@ -2,6 +2,7 @@ package com.bagas.gson;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
@@ -15,34 +16,33 @@ import com.google.gson.JsonObject;
 
 public class DBReader {
 
-//    private final String PATH_TO_DB = "src\\main\\data\\db.json";
-//
-//    private final String PATH_TO_DATA = "src\\main\\data" + File.separator;
-
     private final String PATH_TO_DB = "db.json";
 
-//    private final String PATH_TO_DATA = "data" + File.separator;
+    private final String PATH_TO_DATA = "target" + File.separator + "data" + File.separator;
 
     private JsonObject getJsonRootDB() throws IOException {
-//        try (BufferedReader reader = Files.newBufferedReader(Paths.get(PATH_TO_DB))) {
-//
-//            return GsonConfigurator.getGson().fromJson(reader, JsonObject.class);
+//        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(PATH_TO_DB);
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+//            JsonObject b = GsonConfigurator.getGson().fromJson(reader, JsonObject.class);
+//            System.out.println("getJsonRootDB: " + b);
+//            return b;
 //        }
-        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(PATH_TO_DB);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+        File folder = new File(PATH_TO_DATA);
+        folder.mkdir();
+
+        try (BufferedReader reader = Files.newBufferedReader(
+                Paths.get(PATH_TO_DATA + "db.json"))) {
             JsonObject b = GsonConfigurator.getGson().fromJson(reader, JsonObject.class);
-            System.out.println("getJsonRootDB: " + b);
+      //      System.out.println("getJsonRootDB: " + b);
             return b;
+        } catch (NoSuchFileException ex) {
+            return null;
         }
     }
 
     public JsonArray getJsonArrayByName(String name) throws IOException {
-        System.out.println(getJsonRootDB());
-       // JsonObject data = getJsonRootDB().getAsJsonObject("src/main/data");
         JsonObject data = getJsonRootDB().getAsJsonObject("data");
-        JsonArray asJsonArray = data.getAsJsonArray(name);
-        System.out.println(asJsonArray);
-        return asJsonArray;
+        return data.getAsJsonArray(name);
     }
 
     void initDB(List<String> departments) throws IOException, IncorrectField {
@@ -51,22 +51,11 @@ public class DBReader {
         if (rootDB == null) {
             rootDB = new JsonObject();
             rootDB.add("data", GsonConfigurator.getGson().toJsonTree(new Database()));
-        }
-        System.out.println("initial db: " + rootDB);
-        File directory = new File(File.separator);
-        String[] filesStr = directory.list(new JsonFileFilter(departments));
-      //  String[] filesStr = File.  new JsonFileFilter(departments));
-//        System.out.println("fileStr: ");
-//        Arrays.stream(filesStr).forEach(System.out::println);
 
-//        if (filesStr == null) {
-//            System.out.println("Файлы не найдены");
-//        } else {
-//            for (String s : filesStr) {
-//                moveRecords(s, rootDB);
-//            }
-//        }
-        moveRecords("db_gomel.json", rootDB);
+            for (String department : departments) {
+                moveRecords("db_" + department + ".json", rootDB);
+            }
+        }
     }
 
     private void moveRecords(String fileName, JsonObject targetRoot) throws IOException, IncorrectField {
@@ -80,16 +69,15 @@ public class DBReader {
         }
 
         JsonObject srcDBJson = srcObject.getAsJsonObject("data");
-        System.out.println("srcDBJSON: " + srcDBJson);
+
         srcDBJson.entrySet().forEach(el -> moveElements(el.getValue().getAsJsonArray(),
                 targetDB.get(el.getKey()).getAsJsonArray()));
 
-        try (BufferedWriter writerTarget = Files.newBufferedWriter(Paths.get(PATH_TO_DB))) {
+        try (BufferedWriter writerTarget = Files.newBufferedWriter(Paths.get(PATH_TO_DATA + PATH_TO_DB))) {
             GsonConfigurator.getGson().toJson(targetRoot, writerTarget);
         }
-        System.out.println("targetDB: " + targetDB);
 
-        try (BufferedWriter writerSource = Files.newBufferedWriter(Paths.get(fileName))) {
+        try (BufferedWriter writerSource = Files.newBufferedWriter(Paths.get(PATH_TO_DATA + fileName))) {
             GsonConfigurator.getGson().toJson(srcObject, writerSource);
         }
     }
