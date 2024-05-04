8c2be92996041eee8e23f4446ef04aee5d33aac7
Aleksandr Bogoslovskiy <balics@mail.ru>
2024-05-04 13:14:54 +0300

    leetcode populating-next-right-pointers-in-each-node-2 initial commit, added main.java

---
+++ algorithms/algorithm37/src/Main.java
@@ -0,0 +1,70 @@
+public class Main {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+
+        root.left = node2;
+        root.right= node3;
+
+        node2.left = node4;
+        node2.right = node5;
+
+   //     node3.left = node6;
+        node3.right = node7;
+
+        connect(root);
+
+        System.out.println(root.left.next.val);
+        System.out.println(root.left.left.next.val);
+        System.out.println(root.left.right.next.val);
+    }
+
+    public static Node connect(Node root) {
+       Node dummyHead = new Node(0);
+       Node pre = dummyHead;
+       while(root != null) {
+           if(root.left != null) {
+               pre.next = root.left;
+               pre = pre.next;
+           }
+           if(root.right != null) {
+               pre.next = root.right;
+               pre = pre.next;
+           }
+           root = root.next;
+           if(root == null) {
+               pre = dummyHead;
+               root = dummyHead.next;
+               dummyHead.next = null;
+           }
+       }
+
+       return root;
+    }
+
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
