3751b40c5177039545158fdc376987b7f96155ca
Aleksandr Bogoslovskiy <balics@mail.ru>
2024-05-04 20:01:46 +0300

    use stream and filter

--- algorithms/algorithm19/src/algorithm19/Main.java
+++ algorithms/algorithm19/src/algorithm19/Main.java
@@ -2,32 +2,24 @@ package algorithm19;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(duplicateCount("abcde"));
-		System.out.println(duplicateCount("abcdea"));
+		System.out.println(duplicateCount("abcdeaa"));
 		System.out.println(duplicateCount("Indivisibilities"));
 
 	}
 
 	public static int duplicateCount(String text) {
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		int count = 0;
 		text = text.toLowerCase();
-		
-		for (int i = 0; i < text.length(); i++) {
-			char ch = text.charAt(i);
-			if(map.containsKey(ch)) {
-				if(map.put(ch, map.get(ch) + 1) == 1) {
-					count++;
-				}
-			} else {
-				map.put(ch, 1);
-			}
-		}
-		
-		return count;
+		return (int) text.chars()
+				.mapToObj(ch -> (char) ch)
+				.collect(Collectors.groupingBy(i -> i, Collectors.counting()))
+				.entrySet().stream()
+				.filter(entry -> entry.getValue() >= 2)
+				.count();
 	}
 }
