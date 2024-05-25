3543cdc9f82c94624f74018233554f8ddfe79eaa
Aleksandr Bogoslovskiy <balics@mail.ru>
2024-05-25 11:04:26 +0300

    leetcode maxSubarray initial commit, added main.java .

---
+++ algorithms/algorithm64/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            maxSum = Math.max(currSum, maxSum);
+
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
