768407d3ad6e8e620486a6767c5f9f8007975b71
Aleksandr Bogoslovskiy <balics@mail.ru>
2024-05-27 11:23:25 +0300

    leetcode searchInsert initial commit, added main.java .

---
+++ algorithms/algorithm66/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, 5, 6};
+        System.out.println(searchInsert(nums, 7));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+}
